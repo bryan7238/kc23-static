aac03e3b004275da23eacd4655faf4c0fa01c3ff
Bryan Chung <bryanchung3@gmail.com>
2025-12-01 23:40:04 -0500

    add golf slug

---
+++ out/_next/static/chunks/app/in-memory/page-f572b303f0a0b85c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[303,782,826,238,799,244,765,298,301],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);

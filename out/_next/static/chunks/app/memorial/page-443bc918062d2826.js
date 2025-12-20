f740b55937d5bb512ae766a6ab6c8cdb0df67fe8
Bryan Chung <76828985+bryan7238@users.noreply.github.com>
2025-12-20 11:44:53 -0500

    add quicklink hyperlinks Update officer duties, change Golf Sponsors to Golf Outing, add banner fallback color

--- out/_next/static/chunks/app/memorial/page-443bc918062d2826.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[238,799,826,303,782,765,244,298,301,742],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
